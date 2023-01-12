fd14851636f994454b9112ba5b8134e80a1d32f0
etienne-baroth <etienne.baroth@laplateforme.io>
2023-01-12 14:12:22 +0100

    jour02 rendu job06

---
+++ jour02/job06/index.php
@@ -0,0 +1,29 @@
+<?php
+$largeur = 20;
+$hauteur = 10;
+?>
+
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+<div>
+<style>
+div {
+    border: 2px solid black;
+    width: <?php echo $largeur;?>px;
+    height: <?php echo $hauteur;?>px;
+    }
+</style>
+</div>
+
+</body>
+</html>
+

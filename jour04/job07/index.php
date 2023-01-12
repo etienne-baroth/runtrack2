32af5b03d3accd537feb7d73dbd289158a85af47
etienne-baroth <etienne.baroth@laplateforme.io>
2023-01-12 13:20:45 +0100

    jour04 début job07

---
+++ jour04/job07/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+<form method="post" action="index.php">
+    <input type="text" name="largeur" placeholder="largeur"/>
+    <input type="text" name="hauteur" placeholder="hauteur"/>
+    <input type="submit" name="submit"/>
+</form>
+
+
+</body>
+</html>
+
+<?php
+
+
+?>

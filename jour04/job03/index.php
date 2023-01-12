ff95ca75d9cfea68465f92cc57e50c96f84cbf09
etienne-baroth <etienne.baroth@laplateforme.io>
2023-01-12 11:14:16 +0100

    jour04 rendu job03

---
+++ jour04/job03/index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+<form method="post" action="index.php">
+        <input type="text" name ="prenom" placeholder="prenom"/>
+        <input type="text" name="nom" placeholder="nom"/>
+        <input type="text" name="âge" placeholder="âge"/>
+        <input type="submit" value="Envoyer"/>
+</form>
+
+</body>
+</html>
+
+<?php
+
+$i=0;
+
+foreach ($_POST as $value) {
+    if ($value != "") {
+        $i++;
+    }
+}
+echo 'Le nombre d\'argument POST envoyé est : '. $i;
+
+
+?>

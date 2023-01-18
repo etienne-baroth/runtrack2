68860607d699a0c2fab3007c568f909122b1911e
etienne-baroth <etienne.baroth@laplateforme.io>
2023-01-18 11:18:23 +0100

    job01 correctifs et fonctionne

--- jour08/job01/index.php
+++ jour08/job01/index.php
@@ -2,11 +2,41 @@
 
 session_start();
 
-if(!isset($_SESSION["nbvisites"])) {
-    $_SESSION ["nbvisites"] = 0;
+if (!isset($_SESSION["nbvisites"])){
+    $_SESSION["nbvisites"] = 0;
 }
 
+if(isset($_SESSION["nbvisites"])) {
+    if (isset($_GET["reset"])) {
+        $_SESSION["nbvisites"] = 0;
+        echo $_SESSION["nbvisites"];
+        unset ($_SESSION["nbvisites"]);
+        session_destroy();
+    }
+
 else {
-    $_SESSION ["nbvisites"] = $_SESSION ["nbvisites"] +1;
-    echo $_SESSION ["nbvisites"];
+    echo $_SESSION["nbvisites"];
+    $_SESSION["nbvisites"]++; }
 }
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+<form action="index.php" method="get">
+
+<input type="submit" name="reset" value="reset">
+
+</form>
+
+</body>
+</html>
+

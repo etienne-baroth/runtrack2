56f4ca7fd132ccda23598b79c2e79b2e9c2652ee
etienne-baroth <etienne.baroth@laplateforme.io>
2023-01-19 15:12:50 +0100

    jour09 rendu job10

---
+++ jour09/job10/job10.sql
@@ -0,0 +1,71 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jan 19, 2023 at 03:12 PM
+-- Server version: 10.4.27-MariaDB
+-- PHP Version: 8.2.0
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `jour09`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `etage`
+--
+
+CREATE TABLE `etage` (
+  `id` int(11) NOT NULL,
+  `nom` varchar(255) NOT NULL,
+  `numero` int(11) NOT NULL,
+  `superficie` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `etage`
+--
+
+INSERT INTO `etage` (`SUM(superficie)`) VALUES
+('1000');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `etage`
+--
+ALTER TABLE `etage`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `etage`
+--
+ALTER TABLE `etage`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+
+
+-- Requête SQL
+--  SELECT SUM(superficie) FROM etage;

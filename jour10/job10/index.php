a98a96e231f190a21348eff27a55288e6edefcba
etienne-baroth <etienne.baroth@laplateforme.io>
2023-01-20 12:38:10 +0100

    jour10 rendu job10

---
+++ jour10/job10/index.php
@@ -0,0 +1,46 @@
+<?php
+
+$mysqli = new mysqli('localhost', 'root', '', 'jour09');
+
+if($mysqli -> connect_errno) {
+    echo 'Failed to connect MySQL: '. $mysqli -> connect_error;
+    exit();
+}
+
+$request = $mysqli->query('SELECT* FROM salles ORDER BY capacite');
+$results = $request->fetch_all(MYSQLI_ASSOC);
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+<table border>
+    <thead>
+        <tr>
+            <?php foreach ($results[0] as $key => $value): ?>
+                <th><?= $key ?></th>
+            <?php endforeach; ?>
+        </tr>
+    </thead>
+    <tbody>
+        <?php for($i=0; $i < sizeof($results); $i++):?>
+            <tr>
+                <td><?= $results[$i]["id"] ?></td>
+                <td><?= $results[$i]["nom"] ?></td>
+                <td><?= $results[$i]["id_etage"] ?></td>
+                <td><?= $results[$i]["capacite"] ?></td>
+            </tr>
+        <?php endfor; ?>
+    </tbody>
+</table>
+
+</body>
+</html>
